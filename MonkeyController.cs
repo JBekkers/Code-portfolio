cc2cb48d79b9137d91b17e6eef16b64f1c63867b
esg <60392599+JBekkers@users.noreply.github.com>
2020-08-21 16:47:24 +0200

    Added multiple scripts
    Added multiple scripts

---
+++ MonkeyController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyController : MonoBehaviour
+{
+    private float death;
+    public bool dieing;
+    public Transform Player;
+    public int MoveSpeed = 6;
+    private int MaxDist = 999999;
+    private int MinDist = 0;
+    private int rand;
+
+
+    private void Start()
+    {
+        dieing = false;
+        Player = GameObject.FindWithTag("tree").transform;
+    }
+        void Update()
+    {
+        if (dieing == true)
+        {
+            death -= Time.deltaTime;
+        }
+
+        //if monkey update score times the right multiplier from the combo and roll a dice for a random extra banana ammo drop
+        if (death < 0)
+        {
+            rand = Random.Range(0, 4);
+            Destroy(gameObject);
+            dieing = false;
+            death = 4;
+            ScoreBehavour.instance.Score += 30 + (ScoreBehavour.instance.multiplier * 70);
+        }
+
+        if (rand == 2)
+        {
+            GameObject powerup = Instantiate(monkeySpawner.instance.banananapowerup, transform.position + new Vector3(0,1,0), Quaternion.identity);
+        }
+
+        //move towards player
+        transform.LookAt(Player);
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            ShootController.instance.AmmoCount -= 1;
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.CompareTag("Banana"))
+        {        
+            death = 0.2f;
+            dieing = true;
+        }
+
+        if (col.gameObject.CompareTag("Player") && ShootController.instance.AmmoCount <= 0)
+        {
+            playercontroller.instance.GameEnd = true;
+        }
+    }
+}
---
+++ monkeySpawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class monkeySpawner : MonoBehaviour
+{
+    private GameObject[] Spawnpoints;
+    private GameObject[] monkeys;
+    private GameObject Monkey;
+    private GameObject Picked;
+    public GameObject[] monkeyType;
+    public GameObject banananapowerup;
+    private int monkeyspawned;
+    private float spawntimer;
+    private int rand;
+    private int rmonk;
+    public int wave;
+
+
+    public static monkeySpawner instance { get; private set; }
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        spawntimer = 4;
+        monkeyspawned = 3;
+        wave = 0;
+    }
+    void Update()
+    {
+        Spawnpoints = GameObject.FindGameObjectsWithTag("Spawn");
+        monkeys = (GameObject.FindGameObjectsWithTag("Monkey"));
+
+        spawntimer -= Time.deltaTime;
+
+        if (monkeys.Length <= 0)
+        {
+            for (int i = 0; i < monkeyspawned; i++)
+            {
+                rand = Random.Range(0, Spawnpoints.Length);
+                rmonk = Random.Range(0, monkeyType.Length);
+                Picked = Spawnpoints[rand];
+                Monkey = monkeyType[rmonk];
+                GameObject NewMonkey = Instantiate(Monkey) as GameObject;
+                NewMonkey.transform.position = Picked.transform.position;
+            }
+            monkeyspawned++;
+            wave++;
+        }
+    }
+}
